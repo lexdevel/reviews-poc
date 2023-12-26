2e6adfef413b852b8b15a9556ff0e78bb43ed88d
Oleksandr Lohvinenko <lexdevel@gmail.com>
2023-12-26 10:26:19 -0500

    Upgraded gateway, minor updates.

--- src/gateway/index.js
+++ src/gateway/index.js
@@ -1,17 +1,19 @@
-import { ApolloGateway } from '@apollo/gateway';
+import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 (async () => {
   const gateway = new ApolloGateway({
-    serviceList: [
-      { name: "categories", url: process.env.CATEGORIES_URL },
-      { name: "products", url: process.env.PRODUCTS_URL },
-      { name: "reviews", url: process.env.REVIEWS_URL },
-      { name: "stock", url: process.env.STOCK_URL },
-      { name: "tags", url: process.env.TAGS_URL },
-      { name: "users", url: process.env.USERS_URL },
-    ],
+    supergraphSdl: new IntrospectAndCompose({
+      subgraphs: [
+        { name: "categories", url: process.env.CATEGORIES_URL },
+        { name: "products", url: process.env.PRODUCTS_URL },
+        { name: "reviews", url: process.env.REVIEWS_URL },
+        { name: "stock", url: process.env.STOCK_URL },
+        { name: "tags", url: process.env.TAGS_URL },
+        { name: "users", url: process.env.USERS_URL },
+      ]
+    })
   });
 
   const server = new ApolloServer({
